424194d7808040a2a72d8c9ce295ff94ef3d57b1
Jay Are <jayaregalinada@gmail.com>
2014-03-16 16:58:08 +0800

    Edit Grunt Banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,8 +28,9 @@ module.exports = function (grunt) {
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
     banner: '/*!\n' +
-            ' * Bootstrap v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-            ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * WalisPH Bootstrap v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * from the Twitter Bootstrap v3.1.1 (http://getbootstrap.com)\n' +
+            ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
             ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
             ' */\n',
     jqueryCheck: 'if (typeof jQuery === \'undefined\') { throw new Error(\'Bootstrap\\\'s JavaScript requires jQuery\') }\n\n',
